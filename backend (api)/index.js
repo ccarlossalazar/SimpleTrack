ef2e644ae3ca3b80bb3b8807cfee77e8d1103aff
Los Salazar <cdsalazar2@gmail.com>
2025-02-28 11:22:31 -0800

    m

--- backend (api)/index.js
+++ backend (api)/index.js
@@ -2,7 +2,26 @@ import express from "express"
 import dotenv from "dotenv"
 import sequelize from "sequelize"
 const app = express()
+dotenv.config()
+
+const connect = async () => {
+    try {
+    await sequelize.connect(process.env.MYSQL);
+    console.log("Connected to MYSQL database")
+    } catch (error) {
+    throw error
+    };
+};
+
+sequelize.connection.on("disconnect", () => {
+  console.log("Disconnected from MYSQL database")
+})
+
+sequelize.connection.on("connected", () => {
+    console.log("connected to MYSQL database")
+  })
 
 app.listen(5000, () => {
-  console.log("Connected to backend server")
-})
+    connect()
+  console.log("Connected to backend server");
+});
