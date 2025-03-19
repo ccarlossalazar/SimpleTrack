bd77b6a42dd57620070e7b0fd3b5c2e108741ac4
Los Salazar <cdsalazar2@gmail.com>
2025-03-18 17:14:54 -0700

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,40 @@
 import Navbar from './components/navbar'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-
+function Login() {
   return (
-    <>
-    <Navbar />
-    </>
-  )
+    <div>
+      <h1>Login Page</h1>
+      <Link to="/about">
+        <button>Go to About Page</button>
+      </Link>
+     <p>This is the about page about the rec and stuff</p>
+    </div>
+  );
 }
 
-export default App
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <Link to="/login">
+        <button>Go to Login Page</button>
+      </Link>
+      <p>This is the about page about the rec and stuff</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
